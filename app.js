724080c85118deb6a21ad0d5724b9c0c2552fcb5
hrebon <habibebon90@gmail.com>
2021-02-08 20:42:55 +0600

    upgrade

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const api = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
+
 
 
 const getMealData = meal =>{
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${meal}`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -10,22 +10,24 @@ const getMealData = meal =>{
             alert("food not found")
         }
         else
-            displayMeals(data.meals[0])
+            displayMeals(data.meals)
     })
 }
 
 const displayMeals = food =>{
-
     const mealName = document.getElementById('MealName');
-    const mealDetails = document.createElement('div');
-    mealDetails.className = 'meals'
-    mealDetails.innerHTML = `
-    <button onClick='detailsOfFood("${food.strMeal}")'>
-    <img src = "${food.strMealThumb}">
-    <h3 class="foods-name">${food.strMeal}</h3>
-    </button>
+    food.forEach(foods => {
+        const mealDetails = document.createElement('div');
+        mealDetails.className = 'meals'
+        mealDetails.innerHTML = `
+        <div onClick='detailsOfFood("${foods.strMeal}")'>
+        <img src = "${foods.strMealThumb}" class="shadow  bg-white rounded">
+        <h3 class="foods-name">${foods.strMeal}</h3>
+        </div>
     `
     mealName.appendChild(mealDetails);
+    });
+        
     
 }
 
@@ -42,20 +44,21 @@ const detailsOfFood = details =>{
     .then(data => renderMealsName(data.meals[0]))
 }
 
-const renderMealsName = foods =>{
+const renderMealsName = foodDescribe =>{
     const mealsDetails = document.getElementById('mealsDetails');
     mealsDetails.innerHTML = `
-    <img src = "${foods.strMealThumb}">
-    <h1 class="foods-name">${foods.strMeal}</h1>
+    <img src = "${foodDescribe.strMealThumb}">
+    <h1 class="foods-name">${foodDescribe.strMeal}</h1>
     <h3>Ingredients</h3>
-    <li class="list">${foods.strIngredient1}</li>
-    <li class="list">${foods.strIngredient2}</li>
-    <li class="list">${foods.strIngredient3}</li>
-    <li class="list">${foods.strIngredient4}</li>
-    <li class="list">${foods.strIngredient5}</li>
-    <li class="list">${foods.strIngredient6}</li>
-    <li class="list">${foods.strIngredient7}</li>
-    <li class="list">${foods.strIngredient8}</li>
+    <li class="list">${foodDescribe.strIngredient1}</li>
+    <li class="list">${foodDescribe.strIngredient2}</li>
+    <li class="list">${foodDescribe.strIngredient3}</li>
+    <li class="list">${foodDescribe.strIngredient4}</li>
+    <li class="list">${foodDescribe.strIngredient5}</li>
+    <li class="list">${foodDescribe.strIngredient6}</li>
+    <li class="list">${foodDescribe.strIngredient7}</li>
+    <li class="list">${foodDescribe.strIngredient8}</li>
     `
 }
+
 
